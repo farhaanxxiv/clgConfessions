fe0f1a7cc24581979ff3c6da70069b80930c9f82
farhaanxxiviv <70432375+farhaanxxiviv@users.noreply.github.com>
2021-02-03 11:11:21 +0530

    none

---
+++ init-firebase.js
@@ -0,0 +1,15 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDvVqp7SvQoCGAyoLxWy6cUXyMFRYZ5Cew",
+    authDomain: "vbitconfessions-f747f.firebaseapp.com",
+    projectId: "vbitconfessions-f747f",
+    storageBucket: "vbitconfessions-f747f.appspot.com",
+    messagingSenderId: "410799875260",
+    appId: "1:410799875260:web:c44ce538d8b75dfad75cb9",
+    measurementId: "G-9BZK9WKGF2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
